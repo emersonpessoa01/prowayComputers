7485ac3e15df2140856f97cfdb00ecbad3f4799c
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-02-24 21:54:51 -0300

    updated currency :sparkles:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { NaoEncontradaComponent } from "./nao-encontrada/nao-encontrada.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    NaoEncontradaComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        NaoEncontradaComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/produtos/produtos.module.ts
+++ src/app/produtos/produtos.module.ts
@@ -1,17 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { ProdutosRoutingModule } from './produtos-routing.module';
-import { ProdutosComponent } from './produtos.component';
+import { ProdutosRoutingModule } from "./produtos-routing.module";
+import { ProdutosComponent } from "./produtos.component";
 
+// **************************************************
+import ptBr from "@angular/common/locales/pt";
+import { registerLocaleData } from "@angular/common";
+
+registerLocaleData(ptBr);
+// **************************************************
 
 @NgModule({
-  declarations: [
-    ProdutosComponent
-  ],
-  imports: [
-    CommonModule,
-    ProdutosRoutingModule
-  ]
+    declarations: [ProdutosComponent],
+    imports: [CommonModule, ProdutosRoutingModule],
+    providers: [
+        // ************************************
+        { provide: LOCALE_ID, useValue: "pt" },
+        // ************************************
+    ],
 })
-export class ProdutosModule { }
+export class ProdutosModule {}
