e844fe7230aee1a017135c1a3e95a8ffbcb81752
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-03-02 20:32:37 -0300

    added routerLink :sparkles:

--- src/app/carrinho.service.spec.ts
+++ src/app/carrinho.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { CarrinhoService } from './carrinho.service';
+import { CarrinhoService } from "./carrinho.service";
 
-describe('CarrinhoService', () => {
-  let service: CarrinhoService;
+describe("CarrinhoService", () => {
+    let service: CarrinhoService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CarrinhoService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(CarrinhoService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it("should be created", () => {
+        expect(service).toBeTruthy();
+    });
 });
