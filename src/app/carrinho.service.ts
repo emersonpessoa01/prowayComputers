bec377474ef7cd36b3f5339cdd4cfdf952059d2c
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-03-11 12:48:18 -0300

    updated carrinho service :sparkles:

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -9,14 +9,21 @@ export class CarrinhoService {
     constructor() {}
 
     obterCarrinho() {
-        return JSON.parse(localStorage.getItem("carrinho") || "[]");
+        this.itens = JSON.parse(localStorage.getItem("carrinho") || "[]");
+        return this.itens;
     }
     adicionarAoCarrinho(produto: IProdutoCarrinho) {
         this.itens.push(produto);
         localStorage.setItem("carrinho", JSON.stringify(this.itens));
     }
-    limparCarrinho(){
-      this.itens = [];
-      localStorage.clear();
+    removerProdutoCarrinho(produtoId: number) {
+        this.itens = this.itens.filter((item) => item.id !== produtoId);
+        localStorage.setItem("carrinho", JSON.stringify(this.itens));
+
+        
+    }
+    limparCarrinho() {
+        this.itens = [];
+        localStorage.clear();
     }
 }
